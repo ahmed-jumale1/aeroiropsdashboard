0ad423822daeb9bf46bb57bcb6ee350e1347815e
ahmed-jumale1 <ahmed.jumale+1@aero.com>
2025-02-21 04:05:34 -0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,10 @@ document.addEventListener("DOMContentLoaded", function () {
     header.style.transform = "translate(-50%, 0)";
     header.style.opacity = "0";
 
-    // Animate moving much further up while staying centered
+    // Animate moving further up while staying centered
     setTimeout(() => {
         header.style.transition = "transform 2s ease-out, opacity 2s ease-out";
-        header.style.transform = "translate(-50%, -750%)"; // Move header WAY up
+        header.style.transform = "translate(-50%, -500%)"; // Adjusted movement height
         header.style.opacity = "1";
     }, 500);
 });
